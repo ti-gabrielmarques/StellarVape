9360a3cd5e1b94341d4ee7c58c84fb19ec543842
gabriel.marques <gabriel.marques@agrosatelite.com.br>
2019-09-06 18:27:46 -0300

    Conhecimento do flex

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+"""
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+"""
