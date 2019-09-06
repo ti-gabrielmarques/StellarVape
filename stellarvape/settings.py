5d136435d302ef03b96757f2aba44e0b943eb755
gabriel.marques <gabriel.marques@agrosatelite.com.br>
2019-09-06 16:44:43 -0300

    Adição do painel do usuário no cabeçalho de contato

--- stellarvape/settings.py
+++ stellarvape/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'stellarvape.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,9 +107,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
